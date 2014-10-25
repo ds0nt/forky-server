fc002c8a43fe02f71ba2e5f3f04ee90f889feb41
Daniel Sont <dan.sont@gmail.com>
2014-10-25 15:59:53 +0700

    ShareJS Prototype

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 var debug = require('debug')('http')
 	, express = require('express')
-	, socketIO = require('socket.io')
 	, session = require('express-session')
-	, cookieParser = require('cookie-parser')()
-	, SessionSockets = require('express-session.io')
 	, sessionStore = new session.MemoryStore()
 	, passport = require('passport')
 	, LocalStrategy = require('passport-local').Strategy
 	, bcrypt = require('bcrypt')
 	, util = require('util')
 	, db = require('./lib/db')
+	, cookieParser = require('cookie-parser')()
 	, maps = require('./lib/maps')
 	, bodyParser = require('body-parser');
 
-var gravatar = require('nodejs-gravatar');
+
+
+var sharejs = require('share');
 
 var app = express()
 var server = app.listen(3000, function(){
 	console.log('listening on *:3000');
 });
+
 
 app.use(cookieParser);
 app.use(session({store: sessionStore, secret: 'SECRETZZZ'}));
@@ -29,6 +30,11 @@ app.use(passport.session());
 
 // set up the RethinkDB database
 db.setup();
+var fire = require('./lib/live');
+fire.init(server);
+
+app.use(express.static(sharejs.scriptsDir));
+
 
 passport.use(bodyParser.json());
 passport.use(new LocalStrategy(
@@ -163,119 +169,3 @@ function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
-
-
-
-
-
-
-var io = socketIO(server);
-var sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
-
-var chatcounter = 0;
-
-var sockets = {};
-var boards = {};
-
-var Board = {
-	create: function(board) {
-		console.log('Creating Board: ' + board);
-		boards[board] = {
-			id: board,
-			conns: {}
-		};
-	},
-
-	addSocketToBoard: function(board, socket) {
-		boards[board].conns[socket.id] = socket;
-		socket.user.board = board;
-
-		//Join Channel
-		socket.join('board-' + boards[board].id);
-	},
-
-	message: function(board, msg) {
-		sessionSockets.io.to('board-' + boards[board].id).emit('board message', msg);
-	}
-};
-
-sessionSockets.on('connection', function(err, socket, session) {
-	sockets.id = socket;
-
-	if (typeof session === 'undefined') {
-		console.log('Dropping Sessionless Connection');
-		socket.disconnect();
-		return;
-	}
-
-	socket.user = {
-		username: session.passport.user.mail,
-		board: null
-	};
-
-
-	socket.on('open', function(board) {
-		if (!boards[board]) {
-			Board.create(board);
-		}
-		Board.addSocketToBoard(board, socket);
-
-		console.log("Client " + socket.id + " Connected");
-
-		var data = {
-			id: chatcounter++,
-			title: socket.user.username + ' has joined',
-			board: socket.user.board,
-			typeid: 2,
-			picsrc: gravatar.imageUrl(socket.user.username),
-			timestamp: Date.now(),
-			from: "web"
-		};
-
-		Board.message(socket.user.board, data);
-
-	});
-
-	socket.on('board message', function(msg) {
-		console.log("messaged recived: " + msg);
-		var data = {
-			id: chatcounter++,
-			title: socket.user.username,
-			board: socket.user.board,
-			msg: msg,
-			typeid: 1,
-			picsrc: gravatar.imageUrl(socket.user.username),
-			timestamp: Date.now(),
-			from: "web"
-		};
-
-		Board.message(socket.user.board, data);
-	});
-
-
-	socket.on('disconnect', function () {
-		console.log("Client " + socket.id + " Disconnected");
-
-		if (socket.user.board) {
-			delete boards[socket.user.board].conns[socket.id];
-			var data = {
-				id: chatcounter++,
-				title: socket.user.username + ' has left',
-				board: socket.user.board,
-				typeid: 2,
-				picsrc: gravatar.imageUrl(socket.user.username),
-				timestamp: Date.now(),
-				from: "web"
-			};
-
-			Board.message(socket.user.board, data);
-		}
-
-		delete sockets[socket.id];
-
-
-	});
-
-});
-
-
